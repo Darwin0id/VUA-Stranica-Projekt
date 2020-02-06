d02cae7171b5a505bb98bbe29019811cdf56fe6f
Matija Kolarić <45077123+Darwin0id@users.noreply.github.com>
2020-02-06 19:06:39 +0100

    Prepared for presentation

--- src/controller/navbar.controller.js
+++ src/controller/navbar.controller.js
@@ -3,12 +3,22 @@ export default class NavbarController {
    //KONSTRUKTOR
     constructor(elementID) {
        this.elementID = elementID; //DOHVATI NAVBAR POMOĆU ID-A
+       this.status = 0; //STATUS JE LI OTVOREN ILI ZATVOREN MENI
     }
     
     //OMOGUĆI TOGGLE NAVBARU
     toggleNavbarClick()
     {
-        $("#"+this.elementID).toggle("hidden"); //UZMI ELEMENT KROZ KONSTRUKTOR TE MU TOGLAJ KLASU
+        //FUNKCIJA TOGGLE NE SADRŽI CHECKER ZA BOUNCER-A PA NASTAJE UX BUG; STOGA KORIŠTENA DODATNA VARIJABLA PROVEJRE
+        //
+        if(this.status == 0) //AKO JE STATUS VARIJABLE 0 NAPRAVI ANIMACIJU ZA FADE IN
+        {
+            this.status = 1; //AUTOMATSKI POSTAVI VARIJABLU NA DRUGU RAZINU (JEL JE KORISNIK NAPRAVIO PROMJENU NA UI-u)
+            $("#"+this.elementID).removeClass("hidden").fadeOut(0).fadeIn(500); //UZMI ELEMENT KROZ KONSTRUKTOR TE MU MAKNI KLASU
+        } else {
+            this.status = 0; //AUTOMATSKI POSTAVI VARIJABLU NA PRVU RAZINU (JEL JE KORISNIK NAPRAVIO PROMJENU NA UI-u)
+            $("#"+this.elementID).addClass("hidden"); //UZMI ELEMENT KROZ KONSTRUKTOR TE MU DODAJ KLASU
+        }
     }
 
     //POSTAVI AKTIVNU STRANICU
